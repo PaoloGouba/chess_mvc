daf39640edb7b80b722c26efd9b052bc7f0112ba
PaoloGouba <pfgouba@gmail.com>
2023-01-17 19:15:55 +0100

    define player model, and attributes

---
+++ src/models/players.py
@@ -0,0 +1,18 @@
+class Player():
+    """
+    Player Model according to th e documentation
+    """   
+    
+    def __init__(self, name : str, surname : str, gender : str, rank_pos : int > 0, birthday : str):
+        """
+        __ini__ : 
+            Definition of Player attributes        
+        
+        """
+        self.name = name
+        self.surname = surname
+        self.gender = gender 
+        self.rank = rank_pos
+        self.birthday = birthday
+        
+        return
